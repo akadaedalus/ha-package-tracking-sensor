2e5b3b10e45ec755fc0d97318de215004d3e40db
A.J. Palmer <akadaedalus@gmail.com>
2023-06-20 08:18:36 -0600

    change name from email to package_tracking

---
+++ custom_components/package_tracking/const.py
@@ -0,0 +1,24 @@
+"""Constants for Email Platform."""
+
+DOMAIN = 'package_tracking'
+
+CONF_EMAIL = 'email'
+CONF_PASSWORD = 'password'
+CONF_IMAP_SERVER = 'imap_server'
+CONF_IMAP_PORT = 'imap_port'
+CONF_EMAIL_FOLDER = 'folder'
+CONF_SSL = 'ssl'
+CONF_DAYS_OLD = 'days_old'
+
+ATTR_COUNT = 'count'
+ATTR_TRACKING_NUMBERS = 'tracking_numbers'
+
+EMAIL_ATTR_FROM = 'from'
+EMAIL_ATTR_SUBJECT = 'subject'
+EMAIL_ATTR_BODY = 'body'
+
+USPS_TRACKING_NUMBER_REGEX = r"\b(94\d{20}|\d{4}\s\d{4}\s\d{4}\s\d{4}\s\d{4}\s\d{2})\b"
+UPS_TRACKING_NUMBER_REGEX = r"\b(1Z[A-HJ-NP-Z0-9]{16})\b"
+FEDEX_TRACKING_NUMBER_REGEX = r"\b(\d{12})\b"
+
+EMAIL_DOMAIN_REGEX = r"@([\w.-]+)"
